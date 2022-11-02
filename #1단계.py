05317a768c1a0f8f684c4fd54c78e2e27349dec4
apple010202 <113601994+apple010202@users.noreply.github.com>
2022-11-02 12:58:44 +0900

    1~10장

---
+++ #1단계.py
@@ -0,0 +1,107 @@
+#1단계
+from re import X
+
+
+print('Hello World!')
+
+#2단계
+a = '강한친구 대한민국'
+print(a)
+print(a)
+
+#3번
+A = int(input(''))
+B = int(input(''))
+print(A+B)
+
+#4번
+A = int(input(''))
+B = int(input(''))
+print(A-B)
+
+#5번
+A = int(input(''))
+B = int(input(''))
+print(A*B)
+
+#6번
+A = int(input(''))
+B = int(input(''))
+print(A*B)
+
+#7번
+A = int(input(''))
+B = int(input(''))
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+
+#8번
+a = str(input(''))
+print('a' + '??!')
+
+#9번
+a = int(input(''))
+b = a - 543
+print(b)
+
+#10번
+a = int(input(''))
+b = int(input(''))
+c = int(input(''))
+d = int(input(''))
+e = int(input(''))
+
+if a=1:
+    print(a)
+elif a=0:
+        print('1')
+else:
+    A = -(a - 1)
+    print(A)
+
+if b=1:
+    print(b)
+elif b=0:
+        print('1')
+else:
+    B = -(b - 1)
+    print(B)
+
+if c=2:
+    print(c)
+elif 0<=c<2:
+    C1 = -c + 2
+    print(C1)
+else:
+    C2 = -(c - 2)
+    print(C2)
+
+if d=2:
+    print(d)
+elif 0<=d<2:
+    D1 = -d + 2
+    print(D1)
+else:
+    D2 = -(d - 2)
+    print(D2)
+
+if e=2:
+    print(e)
+elif 0<=e<2:
+    E1 = -e + 2
+    print(E1)
+else:
+    E2 = -(e - 2)
+    print(E2)
+
+if f=8:
+    print(f)
+elif 0<=f<8:
+    F1 = -f + 8
+    print(F1)
+else:
+    F2 = -(f - 2)
+    print(F2)
